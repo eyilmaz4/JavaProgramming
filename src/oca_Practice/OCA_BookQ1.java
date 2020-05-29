8b4fc4e7fe6c79d128e80aac8253376c9d797daf
esra <eyilmaz4@na.edu>
2020-05-29 07:41:00 -0500

    my new project

---
+++ src/oca_Practice/OCA_BookQ1.java
@@ -0,0 +1,11 @@
+package oca_Practice;
+
+public class OCA_BookQ1 {
+
+ private static int $;
+   static String a_b;
+ public static void main(String[] main) {
+
+             System.out.print($);
+            System.out.print(a_b);
+            } }
---
+++ src/oca_Practice/OCA_BookQ2.java
@@ -0,0 +1,15 @@
+package oca_Practice;
+
+public class OCA_BookQ2 {
+
+ public static void main(String[] main) {
+StringBuilder sb1=new StringBuilder();
+sb1.append("ja").append("va");
+String s1="java";
+String s2="java";
+     System.out.println(s1==s2);
+     System.out.println(s1.equals(s2));
+     System.out.println(sb1.toString()==s1);
+     System.out.println(sb1.toString().equals(s1));
+
+            } }
